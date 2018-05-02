68de7ab3c6703a2779a387d70a3eff45650e85ae
Michael Wilson <mwilson@ponywolf.com>
2018-05-02 14:32:06 -0600

    Added joint and autoTrace to physics options

--- com/ponywolf/plugins/joint.lua
+++ com/ponywolf/plugins/joint.lua
@@ -9,10 +9,25 @@ function M.new(instance, map)
   print ("Found pivot: ",instance.name, " connects ", instance.bodyA,  "to", instance.bodyB)
   local bodyA = map:findObject(instance.bodyA)
   local bodyB = map:findObject(instance.bodyB)
-  instance.joint = physics.newJoint(instance.joint or "pivot", bodyA, bodyB, instance.x, instance.y)
+  local jointType = instance.jointType or "distance"
+  
+  
+  if jointType == "distance" then
+    instance.joint = physics.newJoint(jointType, bodyA, bodyB, bodyA.x, bodyA.y, bodyB.x, bodyB.y)
+  elseif jointType == "pivot" or jointType == "weld" then
+    instance.joint = physics.newJoint(jointType, bodyA, bodyB, instance.x, instance.y)
+  elseif jointType == "rope" then
+    instance.joint = physics.newJoint(jointType, bodyA, bodyB)
+  end
+  
+  instance.joint.dampingRatio = instance.dampingRatio
+  instance.joint.frequency = instance.frequency
+  
   instance.isSensor = true
   instance.isVisible = false -- hide rect placeholder
-  instance.alpha = 0 -- hide rect placeholder      
+  instance.alpha = 0.0 -- hide rect placeholder      
+  
+  
   return instance
 end
 
